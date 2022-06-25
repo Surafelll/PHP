b07014f8bf2e1eff32722ac4d200b0c24b77e91a
Suraphel <surafelwondimagegn01@gmail.com>
2022-06-25 02:08:15 -0900

    change

--- demo.php
+++ demo.php
@@ -2,7 +2,11 @@
     <?php
 
     echo "<h1>Hello PHP</h1>";
-    print '<input type=button value=click>  ';
+    print '<input type=button value=click> <br> ';
 
+    $x = 7;
+    $y = 2;
+    $z = $x + $y;
+    echo $z;
     ?>
 </body>
